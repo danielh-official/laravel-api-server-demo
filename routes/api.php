a6509e2071051abd4b1477020e1ed0b7174c410b
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-11 18:05:08 -0500

    Add route name prefix to API route group for better organization

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Api\PartnerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::name('api.')->middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
